70cd6bd20a5d9469d51e30a5923d04697c072c46
dacharat <dacharat.p@ku.th>
2019-05-23 00:43:07 +0700

    :tada: add cors

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require("express");
 require("dotenv").config();
 const db = require("./database");
 const { status_filter, location_trend } = require("./twitter");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 const HOST = "0.0.0.0";
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:7777"]
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("SWP practice API");
