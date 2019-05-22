11cf7b446a14446a4a840a0a6613e402a0583e99
dacharat <dacharat.p@ku.th>
2019-05-22 23:44:02 +0700

    :tada: add README

---
+++ README.md
@@ -0,0 +1,19 @@
+# SWP-Final-Twitter-API
+
+```
+git clone https://github.com/dacharat/swp-twitter-api.git
+cd swp-twitter-api
+```
+Create Twitter project at [TwitterDeveloper]((https://developer.twitter.com/en/apps)), go to `Keys and Tokens` tab and click generate `Access token & access token secret`
+
+create env file by command`touch .env` and copy all key and paste in `.env` 
+
+```
+API_KEY={API key}
+API_SECRET_KEY={API secret key}
+TOKEN_KEY={Access token}
+TOKEN_SECRET_KEY={Access token secret}
+
+```
+
+command `yarn; yarn start` or `npm install; npm start`
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { status_filter, location_trend } = require("./twitter");
 
 const app = express();
 const PORT = 3000;
-const HOST = "127.0.0.1";
+const HOST = "0.0.0.0";
 
 app.get("/", (req, res) => {
   res.send("SWP practice API");
@@ -16,10 +16,10 @@ app.get("/track-filter", async (req, res) => {
   res.send(jsonDb);
 });
 
-status_filter("5ปีคสช");
+// status_filter("5ปีคสช");
 // location_trend(1)
 // 23424960 = woeid of thailand
-// location_trend(23424960);
+location_trend(23424960);
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
